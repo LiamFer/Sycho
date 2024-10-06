efd7ec0feebeee50fbc92a2d4a70f62e9cbc43c9
William Fernandes <wiilfern1910@outlook.com>
2024-10-06 18:20:52 -0300

    Sidebar

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly
 import plotly.graph_objects as go
+import streamviz
 
 st.set_page_config(layout="wide")
 
@@ -12,25 +13,7 @@ def page2():
     FILEPATH = r"C:\\Users\\Windows\\Desktop\\cc.xlsx"
     df = pd.read_excel(FILEPATH)
 
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.dataframe(df)
-
-
-    # Puxando os Alimentos mais consumidos pela Pessoa
-    food = {
-        "Alimentos": [k for i in df["Alimentos Consumidos"].str.split(",").to_numpy() for k in i] ,
-    }
-    food = pd.DataFrame(food)
-    mostConsumedItems = food.groupby("Alimentos")["Alimentos"].count().reset_index(name='count').sort_values(['count'], ascending=False).head(3).to_numpy()
-
-    with col2:
-        col1, col2, col3 = st.columns(3)
-        col1.metric("O Mais Consumido", mostConsumedItems[0][0], f'{mostConsumedItems[0][1]} Vezes', delta_color="off")
-        col2.metric("Segundo mais Consumido", mostConsumedItems[1][0], f'{mostConsumedItems[1][1]} Vezes', delta_color="off")
-        col3.metric("Terceiro mais Consumido", mostConsumedItems[2][0], f'{mostConsumedItems[2][1]} Vezes', delta_color="off")
-
+    # Função pro Gráfico do Plotly
     def create_plotly_graph(data):
         x_values = df["Data e Horário"]
         y_values = df["Quantidade"]
@@ -43,29 +26,84 @@ def page2():
                 y=y_values,
                 mode="lines+markers",
                 marker=dict(size=12, color=colors),
-                line=dict(color="gray"),  # Adjust line color as needed
+                line=dict(color="gray"),
             )
         )
 
         fig.update_layout(
-            title="QTD de Comida Consumida",  # Replace with your desired title
-            xaxis_title="Data",  # Replace with your desired label
-            yaxis_title="Quantidade (KG)",  # Replace with your desired label
+            title="QTD de Comida Consumida",
+            xaxis_title="Data",
+            yaxis_title="Quantidade (KG)",
         )
 
         return fig
 
-    fig = create_plotly_graph(df)
-    st.plotly_chart(fig)
+    col1, col2 = st.columns(2)
 
-
-with st.sidebar:
-    st.logo("marcho_sister.png")
-    pg = st.navigation(
-        [
-            st.Page("page1.py", title="Overview", icon="🔥"),
-            st.Page(page2, title="Second page", icon=":material/favorite:"),
-        ]
+    with col1:
+        streamviz.gauge(
+            0.92,
+            gTitle="KPI Indicator",
+            gSize="MED",
+            sFix="%",
+            gcHigh="#21b581",
+            gcLow="#de8a31",
+            gcMid="#f5d02f",
+            gTheme="#ffffff",
+        )
+    # Puxando os Alimentos mais consumidos pela Pessoa
+    food = {
+        "Alimentos": [
+            k for i in df["Alimentos Consumidos"].str.split(",").to_numpy() for k in i
+        ],
+    }
+    food = pd.DataFrame(food)
+    mostConsumedItems = (
+        food.groupby("Alimentos")["Alimentos"]
+        .count()
+        .reset_index(name="count")
+        .sort_values(["count"], ascending=False)
+        .head(3)
+        .to_numpy()
     )
+
+    with col2:
+        st.header("Ranking Principais Alimentos:")
+        st.divider()
+        col1, col2, col3 = st.columns(3)
+        col1.metric(
+            "O Mais Consumido",
+            mostConsumedItems[0][0],
+            f"{mostConsumedItems[0][1]} Vezes",
+            delta_color="off",
+        )
+        col2.metric(
+            "Segundo mais Consumido",
+            mostConsumedItems[1][0],
+            f"{mostConsumedItems[1][1]} Vezes",
+            delta_color="off",
+        )
+        col3.metric(
+            "Terceiro mais Consumido",
+            mostConsumedItems[2][0],
+            f"{mostConsumedItems[2][1]} Vezes",
+            delta_color="off",
+        )
+        fig = create_plotly_graph(df)
+        st.plotly_chart(fig)
+
+
+st.sidebar.title("Navigation")
+st.sidebar.image("marcho_sister.png", width=100)
+st.sidebar.divider()
+
+pg = st.navigation(
+    {
+        "Escolha uma Opção": [
+            st.Page(page2, title="Overview", icon="📌"),
+            st.Page("page1.py", title="Diário", icon="📝"),
+        ]
+    }
+)
 
 pg.run()
